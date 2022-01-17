7ecc2099a57d333bd364617b9e0960ff2a492bd1
Tochukwu Ewiwilem <tochukwuewiwilem@gmail.com>
2022-01-17 10:26:41 -0800

    initial commit

---
+++ src/app/play/playComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pg-play',
+    template: './playComponent.html',
+    styleUrls: ['./playComponent.css']
+})
+export class PressPlay {
+
+}
